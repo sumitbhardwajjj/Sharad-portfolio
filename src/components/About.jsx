a4142f403454437b034f73282f3e36f514174101
sumitbhardwajjj <135961691+sumitbhardwajjj@users.noreply.github.com>
2024-04-06 20:48:16 +0530

    commit now

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,16 +6,16 @@ const About = () => {
     <section className="about">
       <h1>What I do</h1>
       <h2>
-        I am skilled and passionate Video Editor
+        A professional video editor, content creator. I deliver best quality
+        videos on 4k,2k, any resolution. Edits for YouTube videos, shorts, reels
+        titok documantery
       </h2>
       <video id="background-video" autoPlay muted loop playsInline>
         <source src="HAND.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
       </video>
       <h1 className="about-h1">Show Reel</h1>
       <video id="background-video" autoPlay muted loop playsInline>
         <source src="SHOW.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
       </video>
     </section>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,14 +12,21 @@ const Home = () => {
      <Navbar/>
    <div className='home'>
    <div className='header'>
-    <h2>Hi, I am <b>Sharad Rai</b><br /><span>I am a Video Editor , from Mumbai India.</span></h2>
+    <h2>Video Editor <b>Sharad Rai</b><br /><span> Contact no. +91 9136360356 </span></h2>
+    <div className='icons-name'>
+      <img src="after-effects.png" alt="" />
+      <img src="illustrator.png" alt="" />
+      <img src="youtube.png" alt="" />
+      <img src="photoshop.png" alt="" />
+    </div>
     <button className='contact' onClick={()=>{document.getElementById('Contacts').scrollIntoView({behavior:'smooth'})}}>Contact Me</button>
     <button className='download'>Download CV</button>
   </div>
     <div className='head'>
-      <img src="Group.png" alt="" />
+      <img src="im.jpg" alt="" />
       </div>
     </div>
+   
    </div>
   )
 }
